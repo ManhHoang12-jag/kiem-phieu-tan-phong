c6e5faf390189d5d02978de7fa69a91783f77e4a
ManhHoang12-jag <manhtaybac@gmail.com>
2026-03-11 03:58:14 +0700

    app.py

--- app.py
+++ app.py
@@ -227,7 +227,8 @@ else:
         st.session_state.update({'logged_in': False})
         st.rerun()
         
-    st.markdown("<div style='text-align: center; color: grey; font-size: 12px; margin-top: 30px;'>© 2026 - Bản quyền thuộc UBND Phường Tân Phong</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: grey; font-size: 20px; margin-top: 30px;'>© 2026 - Bản quyền thuộc UBND Phường Tân Phong</div>", unsafe_allow_html=True)
+
 
 
 
